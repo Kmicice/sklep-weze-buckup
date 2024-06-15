16ea220c4cce7bf394d9c3e99a318ed966176ff4
morgenz <pawel.stanislaw.kmita@gmail.com>
2024-06-15 15:16:49 +0200

    init

---
+++ app/public/wp-content/plugins/woocommerce-composite-products/assets/dist/admin/onboarding-task-product-type.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives'), 'version' => '56926a238b65e0b070cd');
---
+++ app/public/wp-content/plugins/woocommerce-composite-products/assets/dist/frontend/checkout-blocks.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wc-blocks-checkout'), 'version' => '6c49d48fca8d21ba57d2');
